5a96fb43bab9eccb18c834aa4993ffd5f7db1669
Andres delaTorre <andresdevlatorre@gmail.com>
2024-06-14 14:20:37 -0500

    casi funcionando todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[index show new create] do
-    resources :bookmarks
+  root 'lists#index'
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
   end
+  resources :bookmarks, only: %i[destroy]
 end
